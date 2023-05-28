10b32dc472281ee72c205e50585cb83b0f4d10b2
Vic <victorialau.dg@gmail.com>
2023-05-27 22:56:03 -0300

    refactor login

---
+++ src/routes/session.router.js
@@ -0,0 +1,62 @@
+import { Router } from "express";
+import UserModel from "../dao/models/user.model.js";
+import { createHash, isValidPassword } from "../utils.js";
+import passport from "passport"
+
+const router = Router()
+
+//Vista para registrar usuarios
+router.get('/register', (req, res) => {
+    res.render('sessions/register')
+})
+
+// API para crear usuarios en la DB
+router.post('/register', passport.authenticate('register', {
+    failureRedirect: '/session/failRegister'
+}), async(req, res) => {
+    res.redirect('/session/login')
+})
+
+router.get('/failRegister', (req, res) => {
+    res.send({ error: 'Failed!'})
+})
+
+// Vista de Login
+router.get('/login', (req, res) => {
+    res.render('sessions/login')
+})
+
+// API para login
+router.post('/login', passport.authenticate('login', {
+    failureRedirect: '/session/failLogin'
+}), async (req, res) => {
+    if (!req.user) {
+        return res.status(400).send({ status: 'error', error: 'Invalid credentials'})
+    }
+
+    req.session.user = {
+        first_name: req.user.first_name,
+        last_name: req.user.last_name,
+        email: req.user.email,
+        age: req.user.age
+    }
+    res.redirect('/products')
+})
+
+router.get('/failLogin', (req, res) => {
+    res.send({ error: 'Fail Login'})
+})
+
+// Cerrar Session
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if(err) {
+            console.log(err);
+            res.status(500).render('errors/base', {error: err})
+        } else res.redirect('/session/login')
+    })
+})
+
+
+
+export default router
